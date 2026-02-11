cc9bdcd61c89cdf646f06fb6607b7c383c67cbba
Dyleto <corentinlemoullec@gmail.com>
2026-02-11 17:26:02 +0100

    fix UX

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -17,11 +17,7 @@ export const toaster = createToaster({
 export const Toaster = () => {
   return (
     <Portal>
-      <ChakraToaster
-        toaster={toaster}
-        insetInline={{ mdDown: "4" }}
-        style={{ marginBottom: "env(safe-area-inset-bottom)" }}
-      >
+      <ChakraToaster toaster={toaster} insetInline={{ mdDown: "4" }}>
         {(toast) => (
           <Toast.Root width={{ md: "sm" }} shadow="none">
             {toast.type === "loading" ? (
