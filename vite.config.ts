da3f8a6ccf67117428238d3e3e2f427b5d92eb4b
Dyleto <corentinlemoullec@gmail.com>
2026-02-19 19:58:04 +0100

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,42 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), ""); // lit .env
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        includeAssets: [
+          "favicon.ico",
+          "apple-touch-icon.png",
+          "masked-icon.svg",
+        ],
+        manifest: {
+          name: "Kettle App",
+          short_name: "Kettle",
+          description: "Plateforme de coaching sportif personnalisé",
+          theme_color: "#18181b",
+          background_color: "#18181b",
+          display: "standalone",
+          icons: [
+            {
+              src: "android-chrome-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "android-chrome-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+      }),
+    ],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
